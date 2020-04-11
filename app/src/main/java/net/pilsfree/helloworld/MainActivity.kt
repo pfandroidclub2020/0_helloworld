4ee45bb2a87a4ce8473027142fc6df24c573b6da
pfandroidclub2020 <qwerty2@students.zcu.cz>
2020-04-11 15:20:40 +0200

    snackbar

--- app/src/main/java/net/pilsfree/helloworld/MainActivity.kt
+++ app/src/main/java/net/pilsfree/helloworld/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             val jmeno = edit.text
             Log.d("HELLOWORLD", jmeno.toString())
+            Snackbar.make(rootlayout,"Ahoj, $jmeno",Snackbar.LENGTH_LONG).show()
             Toast.makeText(this, "Ahoj, $jmeno", Toast.LENGTH_LONG).show()
         }
     }
